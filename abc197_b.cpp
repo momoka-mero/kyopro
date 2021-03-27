de92675ea92df20063db4f7747a9ff959c0e5345
momoka-mero <momokakj8.love@gmail.com>
2021-03-28 00:39:46 +0900

    range_checkをする

---
+++ abc197_b.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <regex>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define Sort(a) sort(a.begin(), a.end())
+
+
+int main(){
+    int h,w,x,y;
+
+    cin >> h >> w >> x >> y;
+    vector<vector<char>> data(h+1, vector<char>(w+1));
+
+    for (int i = 1; i <= h; i++){
+        for (int j = 1; j <= w; j++){
+            cin >> data.at(i).at(j);
+        }
+        
+    }
+
+    int count = 1;
+    //hiku
+    for (int i = x-1; i > 0 ; i--){
+        if(data.at(i).at(y) == '.'){
+            count++;
+        }
+        else{
+            break;
+        }
+    }
+
+    for (int j = y-1; j > 0 ; j--){
+        if(data.at(x).at(j) == '.'){
+            count++;
+        }
+        else{
+            break;
+        }
+    }
+
+    for (int i = x+1; i <= h; i++){
+        if(data.at(i).at(y) == '.'){
+            count++;
+        }
+        else{
+            break;
+        }
+    }
+
+    for (int j = y+1; j <= w; j++){
+        if(data.at(x).at(j) == '.'){
+            count++;
+        }
+        else{
+            break;
+        }
+    }
+    cout << count;
+
+    
+}
